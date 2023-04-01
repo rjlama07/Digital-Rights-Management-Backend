730d023468f81d571bb1b40fa73e990fd699e0fa
rjlama07 <riteshlama5@gmail.com>
2023-04-01 17:36:09 +0545

    feat: error handling for non exissting url

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const vehicalRoute = require("./api/routes/vehical");
 
 app.use("/user", userRoute);
 app.use("/vehical", vehicalRoute);
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "404 page not found",
+  });
+});
 
 app.use((req, res, next) => {
   res.status(200).json({
